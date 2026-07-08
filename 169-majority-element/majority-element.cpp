d3611507aaef67dad3a2ab989ac0da5b7251bbb9
SYNKRONOX <gpchandraditya@gmail.com>
2026-07-08 19:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (32.72%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,30 @@
 class Solution {
-public:
+public:// MOORE's voting algorithm
     int majorityElement(vector<int>& nums) {
-        map<int, int> mpp;
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second > nums.size() / 2){
-                return it.first;
+        int n = nums.size();
+        int cnt = 0;
+        int el;
+        for(int i = 0; i < n; i++){
+            if(cnt == 0){
+                cnt = 1;
+                el = nums[i];// we will have the required element after this for loop
+            }
+            else if(nums[i] == el){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
-        return{};
+        int cnt1 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == el){
+                cnt1++; // this counter is for what we actually for the finally standing soldier elemnt el
+            }
+        }
+        if(cnt1 > n/2){
+            return el;
+        }
+        return {};
     }
 };
