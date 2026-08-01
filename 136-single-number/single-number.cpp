a6bf4241c2ef4ede6766f815a17c4ef19a45e31a
SYNKRONOX <gpchandraditya@gmail.com>
2026-08-01 13:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.35%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor_arr = 0;
+        for(int i = 0; i < nums.size(); i++){
+            xor_arr ^= nums[i];
+        }
+        return xor_arr;
+    }
+};
